4f64778f90587e0b02f7c708ccc6d2fbb715d078
yogevv10 <yogev.v10@gmail.com>
2026-05-06 18:29:11 +0300

    Update column_buckling.py

--- column_buckling.py
+++ column_buckling.py
@@ -27,6 +27,6 @@ def find_critical_load(L, E, A, r, c, e, sigma_allow):
         return sigma_max - sigma_allow
 
     # חסם עליון מבוסס על עומס אוילר התיאורטי
-    p_euler = (np.pi*2 * E * (A * r2)) / (L*2)
+    p_euler = (np.pi**2 * E * (A * r**2)) / (L**2)
     
     return float(bisect(f, 0.01, p_euler * 0.99, xtol=1e-4))
